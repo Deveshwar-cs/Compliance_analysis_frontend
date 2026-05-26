ba1faef25270c3fca947d9b2c8e96a2c15c9e9f0
Deveshwar-cs <goboby744@gmail.com>
2026-05-26 18:24:07 +0530

    update api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5003/api",
+  // baseURL: "http://localhost:5003/api",
+  baseURL: "https://compliance-analysis-backend.onrender.com",
   withCredentials: true,
 });
 
